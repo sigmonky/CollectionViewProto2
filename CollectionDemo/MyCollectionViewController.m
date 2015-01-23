57be1a0f18b567572b9fbea8374c533663ea22a5
sigmonky <sigmonky@gmail.com>
2015-01-23 18:27:54 -0500

    updating repo

--- CollectionDemo/MyCollectionViewController.m
+++ CollectionDemo/MyCollectionViewController.m
@@ -26,7 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    _labels = [@[@"label1",@"label2",@"label3",@"label4",@"label5",@"label6",@"label7",@"label8",@"label9",@"label10",@"label11",@"label12",@"label13",@"label14",@"label15",] mutableCopy];
+    _labels = [@[@"label1",@"label2",@"label3",@"label4",@"label5",@"label6",@"label7",@"label8",@"label9",@"label10",@"label11",@"label12",@"label13",@"label14",@"label15",@"label1",@"label2",@"label3",@"label4",@"label5",@"label6",@"label7",@"label8",@"label9",@"label10",@"label11",@"label12",@"label13",@"label14",@"label15"] mutableCopy];
 }
 
 - (void)didReceiveMemoryWarning
