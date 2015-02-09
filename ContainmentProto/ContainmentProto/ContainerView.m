e094f87d401edd516c612cf158fc0c0459fc6b0a
sigmonky <sigmonky@gmail.com>
2015-02-09 18:10:51 -0500

    adding good containment proto

---
+++ ContainmentProto/ContainmentProto/ContainerView.m
@@ -0,0 +1,21 @@
+//
+//  ContainerView.m
+//  ContainmentProto
+//
+//  Created by Weinstein, Randy - Nick.com on 2/9/15.
+//  Copyright (c) 2015 com.sigmonky. All rights reserved.
+//
+
+#import "ContainerView.h"
+
+@implementation ContainerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
